ea782cf8b60ea55c2963105af0f191c6f19f5c70
avaman7860gla <122087287+avaman7860gla@users.noreply.github.com>
2025-06-11 11:20:59 +0530

    Add files via upload

---
+++ Level1/Handshakes.java
@@ -0,0 +1,9 @@
+
+public class Handshakes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numberOfStudents = input.nextInt();
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+        System.out.println("The maximum number of possible handshakes is " + handshakes);
+    }
+}
