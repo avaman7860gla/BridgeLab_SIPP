6b62c7af626ccdff4f7ea876945e209bfe64a8db
Aman <avaman7860@gmail.com>
2025-06-19 10:10:14 +0530

    [Aman Verma] Add PrimeChecker

---
+++ Extras/PrimeChecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        System.out.println(num + (isPrime(num) ? " is" : " is not") + " a prime number.");
+    }
+
+    static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
