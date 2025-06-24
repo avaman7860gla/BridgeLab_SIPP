de82c7a3de7249027a2e047f11c2fbd88b421f8e
Aman <avaman7860@gmail.com>
2025-06-24 14:01:12 +0530

    [Aman Verma] Add Product class with instance variables and class-level product counter

---
+++ Constructor-Instance-vs-Class-Variable-Access-Modifier-Level1/Instance-vs-class-variable/Product.java
@@ -0,0 +1,27 @@
+class Product {
+    String productName;
+    double price;
+    static int totalProducts = 0;
+
+    Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+        totalProducts++;
+    }
+
+    void displayProductDetails() {
+        System.out.println("Product Name: " + productName + ", Price: ₹" + price);
+    }
+
+    static void displayTotalProducts() {
+        System.out.println("Total Products: " + totalProducts);
+    }
+
+    public static void main(String[] args) {
+        Product p1 = new Product("Laptop", 75000);
+        Product p2 = new Product("Smartphone", 25000);
+        p1.displayProductDetails();
+        p2.displayProductDetails();
+        Product.displayTotalProducts();
+    }
+}
