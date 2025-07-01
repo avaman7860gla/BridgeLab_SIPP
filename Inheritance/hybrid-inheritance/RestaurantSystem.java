c15d5e2bcd15306619713ee9436f3baf41ec7dc3
Aman <avaman7860@gmail.com>
2025-07-01 22:42:08 +0530

    [Aman Verma] Add RestaurantSystem.java

---
+++ Inheritance/hybrid-inheritance/RestaurantSystem.java
@@ -0,0 +1,34 @@
+
+interface Worker {
+    void performDuties();
+}
+
+class Person {
+    String name;
+    int id;
+
+    Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+class Chef extends Person implements Worker {
+    Chef(String name, int id) {
+        super(name, id);
+    }
+
+    public void performDuties() {
+        System.out.println("Cooking food");
+    }
+}
+
+class Waiter extends Person implements Worker {
+    Waiter(String name, int id) {
+        super(name, id);
+    }
+
+    public void performDuties() {
+        System.out.println("Serving customers");
+    }
+}
