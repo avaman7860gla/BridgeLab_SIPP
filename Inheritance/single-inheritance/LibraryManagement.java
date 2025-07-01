ce06361b4fe68515214d2ca65b848d61b07c7b74
Aman <avaman7860@gmail.com>
2025-07-01 22:45:07 +0530

    [Aman Verma] Add LibraryManagement.java

---
+++ Inheritance/single-inheritance/LibraryManagement.java
@@ -0,0 +1,26 @@
+
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int year, String name, String bio) {
+        super(title, year);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    void displayInfo() {
+        System.out.println("Book: " + title + " (" + publicationYear + ")");
+        System.out.println("Author: " + name + " - " + bio);
+    }
+}
