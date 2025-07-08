aab473469e1a5ee4f13068a827de075268654893
Aman <avaman7860@gmail.com>
2025-07-08 16:01:35 +0530

    [Aman Verma] Add StockSpan - Stock Span Problem

---
+++ Stack-Queue-HashMap-and-Hashing-Function/Stack-Queue/StockSpan.java
@@ -0,0 +1,19 @@
+
+import java.util.Stack;
+
+public class StockSpan {
+    public static int[] calculateSpan(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        int[] span = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] <= prices[i]) {
+                stack.pop();
+            }
+            span[i] = (stack.isEmpty()) ? i + 1 : i - stack.peek();
+            stack.push(i);
+        }
+
+        return span;
+    }
+}
